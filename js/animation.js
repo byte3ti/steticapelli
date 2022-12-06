88d7e790aa27d39946be0b4d0b5462467ed4ffc9
Eberson Silva <65686722+EbersonSilva@users.noreply.github.com>
2022-12-06 16:18:04 -0300

    Estilização botoes

--- js/animation.js
+++ js/animation.js
@@ -1,48 +1,41 @@
 const scrollRevealTop = ScrollReveal({
   origin: 'top',
   distance: '100px',
-  duration: 2000,
-  reset: true,
-  
+  duration: 1000,
+  reset: true
 })
 
-scrollRevealTop.reveal('.home, .img2', { interval: 300 });
+scrollRevealTop.reveal('.home, .img2', { interval: 300 })
 
 //////////////////////////////////////////////////////
 
 const scrollRevealLeft = ScrollReveal({
   origin: 'bottom',
   distance: '100px',
-  duration: 2000,
-  reset: true,
- 
-  
- 
+  duration: 1000,
+  reset: true
 })
 
-scrollRevealLeft.reveal('.section1', { interval: 300 });
-
+scrollRevealLeft.reveal('.section1', { interval: 300 })
 
 const scrollRevealFixed = ScrollReveal({
   origin: 0,
   distance: 0,
-  duration: 2000,
+  duration: 1000,
   reset: true,
-  delay: 300,
-  
+  delay: 300
 })
 
-scrollRevealFixed.reveal('.text1, .text2, .impHor, .impCap, .text3' , { interval: 300 });
+scrollRevealFixed.reveal('.text1, .text2, .impHor, .impCap, .text3', {
+  interval: 300
+})
 
 const scrollRevealScale = ScrollReveal({
   origin: 0,
   distance: 0,
-  duration: 2000,
+  duration: 1000,
   reset: true,
   scale: 0
-  
- 
 })
 
-scrollRevealScale.reveal('.img1, video', { interval: 300 });
-
+scrollRevealScale.reveal('.img1, video', { interval: 300 })
